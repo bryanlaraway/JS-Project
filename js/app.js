fb2192eabc487a815c92af5d8e314c10e73a3521
bryanlaraway <bryanlaraway@gmail.com>
2015-11-15 23:50:35 -0800

    Initial layout

---
+++ js/app.js
@@ -0,0 +1,35 @@
+//Functionality needed
+
+
+
+
+//NEW NOTES
+
+//When clicking Save
+    //Check for title
+    //Check for text
+    //If both are present
+        //Move note to Note List
+    //If both aren't present
+        //Indicate with an alert what needs to be fixed
+
+
+//EXISTING NOTES
+
+//Hide text area for existing notes
+
+//When clicking View
+    //Check for presence of title/text in viewer
+    //If no title/text present
+        //Move selected note to main viewer
+        //Remove selected note from Notes List
+
+//When clicking Edit
+    //Check for presence of title/text in viewer
+        //If no title/text present
+        //Move selected note to main viewer
+        //Change viewer to edit mode
+        //Remove selected note from Notes List
+
+//When clicking Delete
+    //Remove line containing the note
